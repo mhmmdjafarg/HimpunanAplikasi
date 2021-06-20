26620dc84adde18d47ad9ef7854d4d76f0724ccc
mhmmdjafarg <mjafarashadiq712@gmail.com>
2021-06-20 19:26:45 +0700

    add icon theme and splashscreen

--- app/src/main/java/com/learn/himpunanapplikasi/SplashActivity.java
+++ app/src/main/java/com/learn/himpunanapplikasi/SplashActivity.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        // langsung pindah ke MainActivity atau activity lain
-        // begitu memasuki splash screen ini
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
     }
 }
